74eecd3bdc75b2d53b6c0237731a9df03b9b5b9f
Dragomir Ivanov <49022669+murhum@users.noreply.github.com>
2020-01-18 15:25:22 +0200

    Update sensor.sh

--- sensor.sh
+++ sensor.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-## To recieve data :
-curl -X GET https://data.sensor.community/airrohr/v1/filter/type=SDS011,country=BG | jq -r '.[].sensordatavalues' > sensordata.txt
+## To transfer data we will use curl command
+curl https://data.sensor.community/static/v2/data.dust.min.json > dust
 
-## To recieve time:
-curl -X GET https://data.sensor.community/airrohr/v1/filter/type=SDS011,country=BG | jq -r '.[].timestamp' > time.txt
+## This one will copy the penultimate values in "txt" file
+sed -E 's/\},\s*\{/\},\n\{/g' dust | grep -m2 'id":10001' -A3 | tail -n2 > sap_sensor
 
-## This one will copy the penultimate time in "txt" file
-cat time.txt | sed -n 2p > second.txt
+## WIth the next two we will copy time and sensor values in "result" file
+cat sap_sensor|jq '.timestamp' > result 2> /dev/null
 
-## The following two will take penultimate values and combine them with time
-cat sensordata.txt | grep -m2 "P1" -B3 | tail -n2 > dust.txt
+cat sap_sensor|jq '.sensordatavalues' >> result 2> /dev/null
 
-cat sensordata.txt | grep -m2 "P2" -B3 | tail -n2 >> dust.txt
+## Let's clean all curly braces
+sed -n -e 1p -e 4,6p -e 9,11p result > final
 
-cat second.txt dust.txt > smoke.txt
-
-echo
-echo "The values are: "
-echo
-
-## This one will show the final result
-cat smoke.txt
+## This two will show the final result
+echo "DATE, TIME AND DUST VALUES OF SENSOR 10001: "
+cat final
 
 ## And now let's clean all the mess we created 
-rm -rf sensordata.txt time.txt second.txt dust.txt smoke.txt
-
-
+rm -rf dust sap_sensor result final
