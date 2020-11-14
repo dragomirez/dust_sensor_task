0029c3055b4fd1eec53f95cb81826875ff29f05b
Dragomir Ivanov <49022669+murhum@users.noreply.github.com>
2020-11-14 19:38:30 +0200

    Update dust_taks.sh

--- dust_taks.sh
+++ dust_taks.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-curl -s https://data.sensor.community/airrohr/v1/sensor/10001/ | jq -s > data.json
-
-cat data.json | grep 'value\|value_type' | tail -n4
+curl -s https://data.sensor.community/airrohr/v1/sensor/10001/ | jq -s | grep 'value\|value_type' | tail -n4
